eb60dc1ba517ce6fc41f03f2a28097ae7f7fd6cf
Paula Silva <paulla.ps83@gmail.com>
2022-02-20 22:35:39 -0300

    Adicionados os testes unitários

--- src/test/java/br/desafio/AppTest.java
+++ src/test/java/br/desafio/AppTest.java
@@ -1,20 +1,21 @@
 package br.desafio;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void validarImpressaoDeTextoEmEscada() {
+        int numero = 10;
+        Assert.assertTrue("Falha ao realizar impressão, pois o número informado é negativo ou zero.", App.imprimirTextoEmEscada(numero));
     }
+
+    @Test
+    public void validarSenhaForte() {
+        String senha = "1Bb!00";
+        Assert.assertTrue("Falha ao validar se a senha informada é forte.", App.validarSenhaForte(senha));
+        System.out.println("A senha informada cumpre todos os requisitos. [ Senha: " + senha + " ]");
+    }
+
 }
